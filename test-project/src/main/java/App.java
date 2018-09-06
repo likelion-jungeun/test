207d45a720509ea6993df5845a60f06c9b17fd36
ko <dpebqkrtm11@naver.com>
2018-09-06 20:36:04 +0900

    클래스활용

--- test-project/src/main/java/App.java
+++ test-project/src/main/java/App.java
@@ -2,39 +2,80 @@ import java.util.Scanner;
 
 public class App {
 
-    static String[] names = new String[10];
-    static int[] ages = new int[10];
-    static String[] ids = new String[10];
-    static String[] depts = new String[10];
+    // 클래스로 나누기
+
+    static class Member {
+        String name;
+        int age;
+        String id;
+        String dept;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getDept() {
+            return dept;
+        }
+
+        public void setDept(String dept) {
+            this.dept = dept;
+        }
+
+    }
 
     static Scanner key = new Scanner(System.in);
     static int index = 0;
 
+    static Member[] members = new Member[10];
+
     public static void inputMembers() {
 
         while (true) {
+            Member m = new Member();
             System.out.println("이름? ");
-            names[index] = key.nextLine();
+            m.setName(key.nextLine());
             System.out.println("나이? ");
-            ages[index] = Integer.parseInt(key.nextLine());
+            m.setAge(Integer.parseInt(key.nextLine()));
             System.out.println("학번? ");
-            ids[index] = key.nextLine();
+            m.setId(key.nextLine());
             System.out.println("학과? ");
-            depts[index] = key.nextLine();
+            m.setDept(key.nextLine());
 
-            index++;
+            members[index++] = m;
             System.out.println("계속 입력하시겠습니까?");
             if ("n".equalsIgnoreCase(key.nextLine()))
                 break;
 
         }
-//인진언니 ㅂㄷㅂㄷ
+
     }
 
     public static void printMembers() {
         System.out.println("이름\t나이\t학번\t학과");
         for (int i = 0; i < index; i++) {
-            System.out.printf("%s\t%d\t%s\t%s\n", names[i], ages[i], ids[i], depts[i]);
+            System.out.printf("%s\t%d\t%s\t%s\n", members[i].getName(), members[i].getAge(), members[i].getId(),
+                    members[i].getDept());
         }
     }
 
