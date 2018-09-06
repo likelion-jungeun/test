cec73f671e3488e9e4d4d4abbc1d9fe383e2ef75
ko <dpebqkrtm11@naver.com>
2018-09-06 17:03:30 +0900

    배열을 이용하여 여러 회원의 정보 저장

--- test-project/src/main/java/App.java
+++ test-project/src/main/java/App.java
@@ -2,27 +2,38 @@ import java.util.Scanner;
 
 public class App {
 
+    // 배열을 이용하여 여러 회원의 정보 저장
     public static void main(String[] args) {
-        // 키보드 입력을 처리할 객체 준비
 
-        Scanner in = new Scanner(System.in);
-        // 사용자로부터 회원 정보 입력받기
+        String[] names = new String[10];
+        int[] ages = new int[10];
+        String[] ids = new String[10];
+        String[] depts = new String[10];
+
+        Scanner key = new Scanner(System.in);
+        int index = 0;
         while (true) {
-
             System.out.println("이름? ");
-            String name = in.nextLine();
+            names[index] = key.nextLine();
             System.out.println("나이? ");
-            int age = Integer.parseInt(in.nextLine());
+            ages[index] = Integer.parseInt(key.nextLine());
             System.out.println("학번? ");
-            String id = in.nextLine();
+            ids[index] = key.nextLine();
             System.out.println("학과? ");
-            String dept = in.nextLine();
+            depts[index] = key.nextLine();
 
-            System.out.printf("%s, %d, %s, %s\n", name, age, id, dept);
-
+            index++;
             System.out.println("계속 입력하시겠습니까?");
-            if ("n".equalsIgnoreCase(in.nextLine()))
+            if ("n".equalsIgnoreCase(key.nextLine()))
                 break;
+
         }
+
+        for (int i = 0; i < index; i++) {
+            System.out.printf("%s %d %s %s\n", names[i], ages[i], ids[i], depts[i]);
+        }
+
+        key.close();
+
     }
 }
